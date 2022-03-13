b29a480f23985c6551b43c3bae3bde1ebb6df2ee
allecosta <alle_costa10@outlook.com>
2022-03-13 17:30:12 -0300

    add footer e header

---
+++ include/footer.php
@@ -0,0 +1,10 @@
+<link href="./css/bootstrap.min.css" rel="stylesheet">
+<link href="./css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,300,600">
+<script src="./js/jquery.min.js" async></script>
+<script src="./js/bootstrap.min.js" async></script>
+<script src="./js/app.js" async></script>
+
+</body>
+
+</html>
---
+++ include/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximun-scale=1">
+    <title>Sistema de Gerenciamento de Login de Usuario</title>
+</head>
+
+<body>
